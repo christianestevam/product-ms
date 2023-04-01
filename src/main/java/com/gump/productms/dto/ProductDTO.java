a4be804a4b0f113a70b0fd756184a29f258d69b7
Christian Estevam <christianestevamb@gmail.com>
2023-04-01 17:30:56 -0300

    fix: modeling the project and Readme.md

---
+++ src/main/java/com/gump/productms/dto/ProductDTO.java
@@ -0,0 +1,11 @@
+package com.gump.productms.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+    private  String name;
+    private  String description;
+    private double price;
+    private boolean available;
+}
