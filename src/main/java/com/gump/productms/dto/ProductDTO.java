96fdac6c2ea6049684068d3ce10ffd8288285fcb
Christian Estevam <christianestevamb@gmail.com>
2023-04-02 14:08:20 -0300

    feat: validation ProductDTO

--- src/main/java/com/gump/productms/dto/ProductDTO.java
+++ src/main/java/com/gump/productms/dto/ProductDTO.java
@@ -2,10 +2,21 @@ package com.gump.productms.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 @Data
 public class ProductDTO {
+
+    @NotBlank
     private  String name;
+
+    @Size(min = 50)
     private  String description;
+
+    @Positive
     private double price;
+
     private boolean available;
 }
