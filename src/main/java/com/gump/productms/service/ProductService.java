cec5942b352df6391cb89800a4eb562bab3df35e
Christian Estevam <christianestevamb@gmail.com>
2023-04-08 14:29:36 -0300

    feat: Services impl

--- src/main/java/com/gump/productms/service/ProductService.java
+++ src/main/java/com/gump/productms/service/ProductService.java
@@ -2,8 +2,12 @@ package com.gump.productms.service;
 
 import com.gump.productms.dto.ProductDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
     Optional<ProductDTO> create(ProductDTO request);
+
+    List<ProductDTO> getAll();
 }
+
--- src/main/java/com/gump/productms/service/ProductServiceImpl.java
+++ src/main/java/com/gump/productms/service/ProductServiceImpl.java
@@ -5,6 +5,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,7 +23,24 @@ public class ProductServiceImpl implements ProductService {
         Product product = mapper.map(request, Product.class);
         repository.saveAndFlush(product);
 
+
+
         ProductDTO response = mapper.map(product, ProductDTO.class);
-        return Optional.of(request);
+
+        return Optional.of(response);
+    }
+
+    @Override
+    public List<ProductDTO> getAll(){
+        ModelMapper mapper = new ModelMapper();
+        List<Product> products = repository.findAll();
+        List<ProductDTO> responses = new ArrayList<>();
+
+        for (Product product : products) {
+            ProductDTO response = mapper.map(product, ProductDTO.class);
+            response.add(response);
+        }
+
+        return responses;
     }
 }
